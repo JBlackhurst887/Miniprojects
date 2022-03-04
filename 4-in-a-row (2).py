37bd33f26e8360e33769cc49f383158c77b295d3
JBlackhurst887 <97898188+JBlackhurst887@users.noreply.github.com>
2022-03-04 21:02:16 +0000

    Update 4-in-a-row (2).py

--- 4-in-a-row (2).py
+++ 4-in-a-row (2).py
@@ -1,55 +1,76 @@
-board = [
-    ["_", "_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_", "_", "_"],
-]
+from re import I
+from shutil import move
+import sys
+
+
+
+#returns a string representation of a board position that can be logged to console.
+def format_user(element):
+    if element is None:
+        return "_"
+    elif element:
+        return "x"
+    else:
+        return "o"
 
 def print_board(board):
     for row in board:
-        for slot in row:
-            print(f"{slot} ", end="")
+        for position in row:
+            print(f"{format_user(position)}", end="")
         print()
 
-#Allows user to exit the application
-def quit(user_input):
+
+def get_user_move():
+#get users move as an input from the console. Validate the number is between 1 - 7 or "q" (for quitting the game).
+    user_input = input("Please enter a position 1 through 7 or enter \"q\" to quit: ")
     if user_input == "q":
-        print("Thank you for playing.")
-        return True
+        print("Thank you for playing. ")
+        sys.exit()
+#validate the number is in range 1 - 7.
+    if user_input in [str(I) in range(1, 8)]:
+        return int(user_input) - 1
     else:
-        return False
+        print("Try again. ")
+        return get_user_move()
 
- #Limiting inputs
- #check if user input is a number
-def check_input(user_input):
-    if not isnum(user_input):
-        return False
-    user_input = int(user_input)
-#check if between 1 and 7
-    if bounds(user_input):
-        return True
+#quit the program    
+#def quit(user_input):
+    #if user_input == "q":
+        #print("Thank you for playing. ")
+        #sys.exit()
 
-def isnum(user_input):
-    if not user_input.isnumeric():
-        print("This is not a valid number.")
-        return False
-    else:
-        return True
 
-def bounds(user_input):
-    if user_input > 7 or user_input < 1:
-        print("That number is out of bounds. Please select a number between 1 and 7. ")
-        return False
-    else: return True
+#def check_input():
+    #get users move as an input from the console. Validate the number is between 1 - 7 or "q" (for quitting the game).
+    #user_input = input("Please enter a position 1 through 7 or enter \"q\" to quit: ")
+#validate the number is in range 1 - 7.
+    #if user_input in [str(i) in range(1, 8)]:
+        #return int(user_input) - 1
+    #else:
+        #return True
 
-while True:
-    print_board(board)
-    user_input = input("Please enter select a column between 1 and 7. Press q to stop playing. ")
-    if quit(user_input):
-        break
-    if not check_input(user_input):
-        print("Please try again. ")
-        continue
+def get_board_coords(move):
+#row is inverted and starts at 0. If user input = 1 then to correlate to row must minus 1
+    row = int(move - 1)
+#col starts at 1 so user input = column selected
+    col = int(move)
+    return row, col
+
+
+def add_to_board(board, user, move):
+#Place user's move onto board
+    row, col = get_board_coords(move)
+    board[row][col] = user
+
+def main():
+    board = [[None for _ in range(7)] for _ in range(6)]
+    user = True
+
+    while True:
+        print_board(board)
+        move = get_user_move()
+
+        add_to_board(board, move, user)
+        
+main()
     
